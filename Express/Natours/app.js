0ae5fcf2f362ce0b5808fce449e8a8552a4418b8
Huzaif Ibrahim <i.huzaif.ibrahim@gmail.com>
2026-01-26 09:06:29 +0530

    Created seperate server.js

--- Express/Natours/app.js
+++ Express/Natours/app.js
@@ -5,11 +5,10 @@ import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
-
 // Middlewares
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
 // This function will be called before the response of all the routes that are defined after this.
 // own MIDDLEWARES
 app.use((req, res, next) => {
@@ -23,15 +22,8 @@ app.use((req, res, next) => {
 // Third-party MIDDLEWARE
 app.use(morgan('dev'))
 
-// Base URL
-app.get("/", (req, res) => {
-    res.send("Hello")
-})
-
 // Middleware to use ROUTER, and also this is called as MOUNTING OF ROUTER
 app.use('/api/v1/tours', tourRouter) // for '/api/v1/tours' we apply tourRouter middleware
 app.use('/api/v1/users', userRouter) // for '/api/v1/users' we apply userRouter middleware
 
-app.listen(3000, () => {
-    console.log("App is listening to requests on port 3000.")
-})
+export default app
